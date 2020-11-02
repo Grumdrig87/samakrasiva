bd5fde712335314fe90a1c68cf5cd68e65d2d0c0
Вит ноут <grumdrig@mail.ru>
2020-11-02 12:37:57 +0300

    h

---
+++ js/main.js
@@ -0,0 +1,58 @@
+jQuery(document).ready(function($){    
+  
+
+  // var $slider = $('[data-slider]');
+	// if($slider.length>0){
+	// 	$slider.slick({
+	// 		dots: true,
+	// 		speed: 300,
+	// 		slidesToShow: 3,
+	// 		responsive: [{
+	// 			breakpoint: 1200,
+	// 			settings: {
+	// 				slidesToShow: 2
+	// 			}
+	// 		}, {
+	// 			breakpoint: 993,
+	// 			settings: {
+	// 				slidesToShow: 1,
+	// 				centerMode: true
+	// 			}
+	// 		}]
+	// 	});
+	// }
+    
+    
+    // // faq
+    // $('[data-faq]').click(function(){
+    //   $(this).toggleClass('open');
+    //   $(this).find('.faq__card-help').toggleClass('open');
+    //   $(this).find('p').slideToggle(300);
+    // })
+      // adaptive
+
+    //   if ($(window).width() < 993) {
+    //     // burger
+    //     $('[data-burger]').click(function(){
+    //       $('body').toggleClass("open");
+    //       $(this).toggleClass("open");
+    //       $('[data-nav]').toggleClass("open");
+    //     });
+    //     //header menu 
+          
+    //     function closeMenu () {
+    //       $(document).mouseup(function (e){ // событие клика по веб-документу
+    //         var div = $("[data-nav]"); // тут указываем ID элемента
+    //         if (!div.is(e.target) // если клик был не по нашему блоку
+    //           && div.has(e.target).length === 0 && !$('[data-burger]').is(e.target)) { // и не по его дочерним элементам
+    //           div.removeClass('open');
+    //           $('[data-burger]').removeClass("open");
+    //           $('body').removeClass('open');
+    //         }
+    //       });
+    //     }
+
+    //     closeMenu();
+        
+    // }
+})
